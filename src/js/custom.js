ea15889fb766e792980c8e40f3b7c357e6b0c197
JackyJack-w <beskapac@gmail.com>
2021-06-10 09:31:08 +0300

    save progress

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,8 +1,33 @@
+$(document).ready(function() {
+
+    console.log('document');
+
+    $("label").click(function(){
+        console.log('i label click');
+        $("label").closest().addClass("checked");
+      });
+    
+
+
+});
 console.log('DOM ok');
 
-function checkLabel(){
-
-    console.log('i see click');
-
-    $('.input-check').css('background','red');
+function checkLabel(elem) {
+    var a = document.getElementsByTagName('label')
+    for (i = 0; i < a.length; i++) {
+        a[i].classList.remove('checked');
+    }
+    elem.classList.add('checked');
 }
+
+function menuBtn(){
+
+    let elem = document.getElementById("main-menu");
+    elem.style.display = 'block';
+    let close = document.getElementById("close-menu");
+    close.onclick = function(){
+        elem.style.display = 'none';
+    };
+}
+
+
